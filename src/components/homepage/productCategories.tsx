e6d9bb7ee9e344bb20da3fd8da7fc2ba9ff01b4b
KenwardKH <kenwardkh5@gmail.com>
2026-01-08 00:48:19 +0700

    footer

--- src/components/homepage/productCategories.tsx
+++ src/components/homepage/productCategories.tsx
@@ -38,38 +38,38 @@ export default function ProductCategories() {
       id: "tools",
       img: alat,
       slug: "tools",
-    }
+    },
   ];
   return (
-    <section className="py-16 container mx-auto px-6 ">
-      <div className="text-center mb-12">
-        <h1 className="text-4xl font-bold mb-2">{t("categories.title")}</h1>
-        <h2 className="text-2xl font-semibold">
-          {t("categories.subtitle")}
-        </h2>
-      </div>
-      <div className="w-full flex justify-center">
-        <div className="flex flex-wrap justify-center gap-3 md:gap-8 max-w-7xl">
-          {categoryList.map((item) => (
-            <Link
-              to="#"
-              className="group block border rounded-lg overflow-hidden shadow-md transition-all hover:shadow-xl hover:scale-105 duration-300"
-            >
-              <div className="w-64 h-40 md:w-72 md:h-64 overflow-hidden relative">
-                <img
-                  src={item.img}
-                  alt={item.id}
-                  className="w-full h-full object-cover group-hover:scale-105 transform duration-500"
-                />
-                <div className="absolute inset-0 bg-black/10 group-hover:bg-black/0 transform duration-500"></div>
-              </div>
-              <div className="text-center">
-                <p className="px-4 py-2 font-semibold group-hover:text-yellow-600 duration-400">
-                  {t(`categories.${item.id}`)}
-                </p>
-              </div>
-            </Link>
-          ))}
+    <section className="py-16 bg-gray-100">
+      <div className="container mx-auto px-6">
+        <div className="text-center mb-12">
+          <h1 className="text-4xl font-bold mb-2">{t("categories.title")}</h1>
+          <h2 className="text-2xl font-semibold">{t("categories.subtitle")}</h2>
+        </div>
+        <div className="w-full flex justify-center">
+          <div className="flex flex-wrap justify-center gap-3 md:gap-8 max-w-7xl">
+            {categoryList.map((item) => (
+              <Link
+                to="#"
+                className="group block border rounded-lg overflow-hidden shadow-md transition-all hover:shadow-xl hover:scale-105 duration-300"
+              >
+                <div className="w-64 h-40 md:w-72 md:h-64 overflow-hidden relative">
+                  <img
+                    src={item.img}
+                    alt={item.id}
+                    className="w-full h-full object-cover group-hover:scale-105 transform duration-500"
+                  />
+                  <div className="absolute inset-0 bg-black/10 group-hover:bg-black/0 transform duration-500"></div>
+                </div>
+                <div className="text-center bg-white">
+                  <p className="px-4 py-2 font-semibold group-hover:text-yellow-600 duration-400">
+                    {t(`categories.${item.id}`)}
+                  </p>
+                </div>
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     </section>
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -3,7 +3,7 @@ import LanguageSwitcher from "./switchLanguage";
 import { useState } from "react";
 import { IoMdMenu } from "react-icons/io";
 import { FiX } from "react-icons/fi";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,21 +11,26 @@ export default function Navbar() {
   const { t } = useTranslation();
   return (
     <header className="sticky left-0 top-0 z-10">
-      <nav className="bg-white shadow-md w-full">
+      <nav className="shadow-md w-full bg-transparent">
         <div className="max-w-7xl mx-auto px-6">
           <div className="flex justify-between h-16 items-center">
-            <h1>Suburmaju</h1>
+            <h1 className="text-xl font-bold uppercase tracking-wide">
+              <Link to="/">Subur maju</Link>
+            </h1>
 
             <div className="hidden md:flex space-x-6 text-lg">
-              <Link to="#" className="hover:text-blue-500">
+              <NavLink to="/" className="hover:text-blue-500 font-semibold transition duration-300">
+                {t("nav.home")}
+              </NavLink>
+              <NavLink to="about" className="hover:text-blue-500 font-semibold transition duration-300">
                 {t("nav.about")}
-              </Link>
-              <Link to="#" className="hover:text-blue-500">
+              </NavLink>
+              <NavLink to="" className="hover:text-blue-500 font-semibold transition duration-300">
                 {t("nav.products")}
-              </Link>
-              <Link to="#" className="hover:text-blue-500">
+              </NavLink>
+              <NavLink to="#" className="hover:text-blue-500 font-semibold transition duration-300">
                 {t("nav.contact")}
-              </Link>
+              </NavLink>
             </div>
             <div className="hidden md:flex">
               <LanguageSwitcher />
